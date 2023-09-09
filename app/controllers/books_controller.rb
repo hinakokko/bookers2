1b804315e7fc0ce642b29738ce07372f739f650c
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-09-09 09:56:13 +0000

    bookers2_課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,19 @@ class BooksController < ApplicationController
     @books = Books.all
   end
 
+  def new
+    @books = Books.new
+  end
+
   def show
   end
+
+  def create
+    @books = Books.new(books_params)
+    @books.user_id = current_user.id
+    @books.save
+  end
+
+
 
 end
